60a18187c57586b7c832a08a5efa9bec581c949b
Frank Richard Semakula <franksemakula@gmail.com>
2018-03-12 17:08:56 +0100

    Done. Needs to add localstorage

--- src/main/movies/MovieListItem.js
+++ src/main/movies/MovieListItem.js
@@ -2,16 +2,16 @@
 
 import React from "react";
 import "./MovieListItem.css";
-
+import { Link } from "react-router-dom";
 
 const MovieListItem = ({ movie }) => {
-    const { title, poster_path, release_date, vote_average } = movie;
-    // fetches the 'title' and 'poster_path' from the object
+    const { id, title, poster_path, release_date, vote_average } = movie;
     const imgUrl = `https://image.tmdb.org/t/p/w342/${poster_path}`
     const year = release_date.substring(0, 4)
 
     return (
         <li className="movie-item">
+            <Link to={`/movies/${id}`} className="thumbnail">
             <img src={imgUrl} alt={title}/>
             <div className="movie-description">            
                 <h2>{title}</h2>
@@ -26,6 +26,7 @@ const MovieListItem = ({ movie }) => {
                 </div>
                 </section>
             </div>
+            </Link>
         </li>
     )
 }
